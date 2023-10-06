79eefbf628064be61b35e633fb87828c6e9c6b44
emma-carballal <carballal.emma@gmail.com>
2023-10-06 12:16:35 +0200

    Delete input prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-if prompt := st.chat_input("Wie geht's?"):
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
